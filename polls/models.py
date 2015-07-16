754721c921179f20b2f12f2b9e8f89af2b399318
Joe Ardent <code@ardent.nebcorp.com>
2015-07-15 22:04:02 -0700

    bugfixes

--- polls/models.py
+++ polls/models.py
@@ -19,5 +19,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __unicode__():
+    def __unicode__(self):
         return self.choice_text
